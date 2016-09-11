902db5ce3d8201602173abc8299cd5157a10618f
Neto Deolino <netodeolino@gmail.com>
2016-09-11 13:50:07 -0300

    Versão 0.0.1

---
+++ aluno/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+ * O sistema deve cadastrar alunos, suas notas e retornar
+ * a media de nota por aluno e de todos alunos cadastrados
+ *
+ * Voce deve usar para este sistema: Vector, For, If/Else e
+ * Struct pelo menos!
+*/
+
+int main(int argc, char *argv[])
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
