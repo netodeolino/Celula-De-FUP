9025505ca2e9618924e24e0cc3938f21ff9ed131
Neto Deolino <netodeolino@gmail.com>
2016-09-21 17:59:18 -0300

    correção aluno

--- sistema_cadastro/main.cpp
+++ sistema_cadastro/main.cpp
@@ -2,15 +2,14 @@
 #include <vector>
 using namespace std;
 
-struct negada{
+struct aluno {
     string nome;
     int idade;
     string curso;
     int matricula;
 };
 
-
-void procurar_aluno(vector <negada> turma){
+void procurar_aluno(vector <aluno> turma){
     int matricula_procurada;
     cout << "digite o numero de matricula" << endl;
     cin >> matricula_procurada;
@@ -24,38 +23,38 @@ void procurar_aluno(vector <negada> turma){
     }
 }
 
-void imprimir_negada(vector <negada> turma){
+void imprimir_alunos(vector <aluno> turma){
     for(int i = 0; i < turma.size(); i++){
         cout << turma[i].nome << endl;
     }
 }
 
-negada salvar_negada(){
-    negada aluno;
+aluno salvar_aluno(){
+    aluno alu;
     cout << "qual teu nome mah?"<< endl;
-    cin >> aluno.nome;
+    cin >> alu.nome;
     cout << "qual a tua idade?"<< endl;
-    cin >> aluno.idade;
+    cin >> alu.idade;
     cout << "qual seu numero de matricula?" << endl;
-    cin >> aluno.matricula;
+    cin >> alu.matricula;
     cout << "faz o que da vida na ufc?" << endl;
-    cin >> aluno.curso;
+    cin >> alu.curso;
 
-    return aluno;
+    return alu;
 }
 
 int main()
 {
-    int numero_pessoas = 0;
+    int numero_pessoas;
     cout <<"quantas pessoas voce quer salvar na lista?" << endl;
     cin >> numero_pessoas;
 
-    vector <negada> turma;
+    vector <aluno> turma;
 
     for(int i = 0 ; i < numero_pessoas ; i++){
-        turma.push_back(salvar_negada());
+        turma.push_back(salvar_aluno());
     }
-    imprimir_negada(turma);
+    imprimir_alunos(turma);
     procurar_aluno(turma);
 
     return 0;
